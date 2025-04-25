f2a7a4e11d953ca80c0fa3e7d0ce0e50a17b79ed
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2025-04-25 12:40:21 +0530

    some d change

--- controllers/React.js
+++ controllers/React.js
@@ -1,4 +1,5 @@
 const path = require('path')
+console.log(path.resolve(__dirname,'..',"client","build","index.html"))
 const reactPage = async (req,res)=>{
     try {
         res.sendFile(path.resolve(__dirname,'..',"client","build","index.html"))
